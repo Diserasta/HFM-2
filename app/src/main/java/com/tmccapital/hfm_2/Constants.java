6f76150e24282daa63a38633bcb05b2688829d81
Sami <diserasta@gmail.com>
2015-09-01 17:10:07 +1000

    Seperated bluetooth into own service.

---
+++ app/src/main/java/com/tmccapital/hfm_2/Constants.java
@@ -0,0 +1,20 @@
+package com.tmccapital.hfm_2;
+
+import android.bluetooth.BluetoothDevice;
+
+/**
+ * Created by Sally on 1/09/2015.
+ */
+public interface Constants {
+
+    public static final String TAG = "HFM";
+
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+}
--- app/src/main/java/com/tmccapital/hfm_2/DispenseFuel.java
+++ app/src/main/java/com/tmccapital/hfm_2/DispenseFuel.java
@@ -1,6 +1,6 @@
 package com.tmccapital.hfm_2;
 
-import android.app.Dialog;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -10,8 +10,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,15 +21,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
 
+import com.tmccapital.hfm_2.Constants;
 
 public class DispenseFuel extends AppCompatActivity {
 
@@ -38,53 +43,43 @@ public class DispenseFuel extends AppCompatActivity {
         return strDate;
     }
 
-    private ArrayAdapter<BluetoothDevice> mArrayAdapter;
-    private Spinner devSpinner;
-    private BluetoothAdapter btAdapter;
+    private Button scan_btn;
+    private String mConnectedDeviceName = null;
+    private String mChosenAddress;
+    private ArrayAdapter<String> mConversationArrayAdapter;
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    BluetoothService mBluetoothService;
 
-    // Create a BroadcastReceiver for ACTION_FOUND
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                // Add the name and address to an array adapter to show in a ListView
-                mArrayAdapter.add(device);
-                mArrayAdapter.notifyDataSetChanged();
-                mArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                if (devSpinner !=null){
-                    devSpinner.setAdapter(mArrayAdapter);
-                }
-            }
-        }
-    };
+    /**
+     * String buffer for outgoing messages
+     */
+    private StringBuffer mOutStringBuffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispense_fuel);
 
-        //Let's instantiate our spinners
-        devSpinner = (Spinner) findViewById(R.id.dispense_dev_spinner);
+        //Let's instantiate our button
+        scan_btn = (Button) findViewById(R.id.dispense_scan_btn);
+        scan_btn.setOnClickListener(new View.OnClickListener(){
 
-        //Let's try and discover any HFM devices
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (btAdapter == null){
-            System.exit(400); //Your device doesn't support bluetooth - get a new one
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(DispenseFuel.this, ListActivity.class);
+                startActivityForResult(intent, 1);
+                String address = intent.getExtras().getString(ListActivity.EXTRA_DEVICE_ADDRESS);
+                mChosenAddress = address;
+            }
+        });
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            this.finish();
         }
-        if (!btAdapter.isEnabled()){
-            Intent enableBtIntent = new Intent (BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        if (mBluetoothService == null){
+            setupBT();
         }
-
-        // Register the BroadcastReceiver
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-
-        btAdapter.startDiscovery();
-
-
         Button go = (Button)findViewById(R.id.dispense_disp_btn);
         go.setOnClickListener(new View.OnClickListener() {
 
@@ -106,7 +101,7 @@ public class DispenseFuel extends AppCompatActivity {
                 transInfo = transInfo + "Make: " + make.getText().toString() + " Model: " + model.getText().toString() + "\n";
                 transInfo = transInfo + "Odo: " + odo.getText().toString() + " " + uom.getText().toString() + "\n";
                 transInfo = transInfo + "Notes: \n" + notes.getText().toString();
-                transInfo = transInfo + "Going to Spinner Page!\n";
+                transInfo = transInfo + "Attempting Bluetooth Connection\n";
 
                 try {
                     FileOutputStream outStream = openFileOutput("log", Context.MODE_PRIVATE);
@@ -117,9 +112,13 @@ public class DispenseFuel extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
+
+
                 //Let's attempt a connection
-                ConnectThread conn = new ConnectThread((BluetoothDevice) devSpinner.getSelectedItem());
-                conn.start();
+                BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(mChosenAddress);
+                mBluetoothService.connect(device, false);
+
+                //Begin Data Transfer
 
 
                 Intent intent = new Intent(DispenseFuel.this, DispenseSpinner.class);
@@ -150,134 +149,62 @@ public class DispenseFuel extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-
-        unregisterReceiver(mReceiver);
-    }
-
-
     private final Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            byte[] writeBuf = (byte[]) msg.obj;
-            int begin = (int)msg.arg1;
-            int end = (int)msg.arg2;
-            switch(msg.what) {
-                case 1:
+            FragmentActivity activity = DispenseFuel.this;
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothService.STATE_CONNECTED:
+
+                            break;
+                        case BluetoothService.STATE_CONNECTING:
+
+                            break;
+                        case BluetoothService.STATE_LISTEN:
+                        case BluetoothService.STATE_NONE:
+
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
                     String writeMessage = new String(writeBuf);
-                    writeMessage = writeMessage.substring(begin, end);
+                    mConversationArrayAdapter.add("SEND:  " + writeMessage);
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    Toast.makeText(activity, "Connected to "
+                            + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                            Toast.LENGTH_SHORT).show();
                     break;
             }
         }
     };
 
-    private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
-        private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        public ConnectThread(BluetoothDevice device) {
-            // Use a temporary object that is later assigned to mmSocket,
-            // because mmSocket is final
-            BluetoothSocket tmp = null;
-            mmDevice = device;
+    private void setupBT(){
+        Log.d(Constants.TAG,"setupBT()");
 
-            // Get a BluetoothSocket to connect with the given BluetoothDevice
-            try {
-                // MY_UUID is the app's UUID string, also used by the server code
-                tmp = device.createRfcommSocketToServiceRecord(UUID.fromString("00009869-0000-1000-8000-00805F9B34FB"));
-            } catch (IOException e) { }
-            mmSocket = tmp;
-        }
+        mConversationArrayAdapter = new ArrayAdapter<String>(DispenseFuel.this, R.layout.message);
 
-        public void run() {
-            // Cancel discovery because it will slow down the connection
-            mBluetoothAdapter.cancelDiscovery();
+        // Initialize the BluetoothChatService to perform bluetooth connections
+        mBluetoothService = new BluetoothService(DispenseFuel.this, mHandler);
 
-            try {
-                // Connect the device through the socket. This will block
-                // until it succeeds or throws an exception
-                mmSocket.connect();
-            } catch (IOException connectException) {
-                // Unable to connect; close the socket and get out
-                try {
-                    mmSocket.close();
-                } catch (IOException closeException) { }
-                return;
-            }
-
-            ConnectedThread mConnectedThread = new ConnectedThread(mmSocket);
-            mConnectedThread.start();
-        }
-
-        /** Will cancel an in-progress connection, and close the socket */
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
+        // Initialize the buffer for outgoing messages
+        mOutStringBuffer = new StringBuffer("");
     }
-
-    private class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            // Get the input and output streams, using temp objects because
-            // member streams are final
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) { }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            byte[] buffer = new byte[1024];
-            int begin = 0;
-            int bytes = 0;
-            while (true) {
-                try {
-                    bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
-                    for(int i = begin; i < bytes; i++) {
-                        if(buffer[i] == "|".getBytes()[0]) {
-                            mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
-                            begin = i + 1;
-                            if(i == bytes - 1) {
-                                bytes = 0;
-                                begin = 0;
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    break;
-                }
-            }
-        }
-
-        /* Call this from the main activity to send data to the remote device */
-        public void write(byte[] bytes) {
-            try {
-                mmOutStream.write(bytes);
-            } catch (IOException e) { }
-        }
-
-        /* Call this from the main activity to shutdown the connection */
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
-    }
-
 }
 
